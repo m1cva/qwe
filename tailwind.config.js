1ef5a01112a781187983118b24ea1c25fe46fb3f
m1cva <pu11p33@yandex.ru>
2025-05-29 00:02:12 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'gray': '#ADADAD',
+            },
+            screens: {
+                'xl': '1500px',
+                'l': '800px',
+            },
+        },
+    },
+    plugins: [],
+}
